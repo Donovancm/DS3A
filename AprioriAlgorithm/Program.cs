806c4ac7e9801e3f90d13626ce9c2d115811fa99
Donovancm <donovan.dcm@gmail.com>
2019-05-13 11:38:42 +0200

    Table in command line

--- AprioriAlgorithm/Program.cs
+++ AprioriAlgorithm/Program.cs
@@ -8,61 +8,94 @@ namespace AprioriAlgorithm
 {
     class Program
     {
-        public static double countXuY= 0;
+        public static double countXuY = 0;
         public static double countX = 0;
         public static double countY = 0;
         public static double n = 0;
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Apriori Algorithm!");
 
             Dictionary<int, string[]> transactions = new Dictionary<int, string[]>();
-            transactions.Add(1, new string[] { "Milk", "Bread"});
+            transactions.Add(1, new string[] { "Milk", "Bread" });
             transactions.Add(2, new string[] { "Butter" });
             transactions.Add(3, new string[] { "Beer" });
-            transactions.Add(4, new string[] { "Milk", "Bread", "Butter"});
+            transactions.Add(4, new string[] { "Milk", "Bread", "Butter" });
             transactions.Add(5, new string[] { "Bread" });
+            TableMatrix(transactions);
 
             string Y = "Milk";
             string[] X = { "Butter", "Bread" };
-            n = transactions.Count;
-            double support = CalculateSupport(transactions,Y, X);
-            double confidence = CalculateConfidence(transactions,Y,X);
-            double lift = CalculateLift(transactions, Y, support);
-            //PrintTableMatrix(transactions);
 
+            n = transactions.Count;
+            double support = CalculateSupport(transactions, Y, X);
+            double confidence = CalculateConfidence(transactions, Y, X);
+            double lift = CalculateLift(transactions, Y, support);
+            
+           
             Console.ReadLine();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
-        //public static void PrintTableMatrix(Dictionary<int, string[]> transactions)
-        //{
-        //    string[] headers = new string[] { "Transactions", "Milk","Bread","Butter","Beer" };
-        //    double[,] array = new double[headers.Length, transactions.Count];
-        //    //magic
-        //    int row = array.GetLength(0);
-        //    int col = array.GetLength(1);
-        //    for (int i = 0; i < row; i++)
-        //    {
-        //        int key = i + 1;
-        //        string[] value = transactions[key];
-        //        // check and fill matrix
-        //        for (int j = 0; j <= col -1; j++)
-        //        {
-        //            if (j ==0 && array[i, j] == 0)
-        //            {
-        //                array[i, j] = key;
-        //            }
-        //            else if (headers[j] == value[j])
-        //            {
-                     
-        //                array[i, j] = 1;
-        //            }
-        //        }
-        //    }
-        //}
+        public static void TableMatrix(Dictionary<int, string[]> transactions)
+        {
+            string[] headers = new string[] { "Transactions", "Milk", "Bread", "Butter", "Beer" };
+            double[,] array = new double[headers.Length, transactions.Count];
+            //magic
+            int row = array.GetLength(0);
+            int col = array.GetLength(1);
+            for (int i = 0; i < row; i++)
+            {
+                int key = i + 1;
+                string[] value = transactions[key];
+                // check and fill matrix
+                for (int j = 0; j <= col - 1; j++)
+                {
+                    if (j == 0 && array[i, j] == 0)
+                    {
+                        array[i, j] = key;
+                    }
+                    for (int k = 0; k < value.Length; k++)
+                    {
+                        if (j > 0 && headers[j] == value[k])
+                        {
+                            array[i, j] = 1;
+                        }
+                    }
+
+                }
+            }
+            PrintTableMatrix(array);
+        }
+
+        public static void PrintTableMatrix(double[,] array)
+        {
+            string[] headers = new string[] { "Transactions", "Milk", "Bread", "Butter", "Beer" };
+            int row = array.GetLength(0);
+            int col = array.GetLength(1);
+
+            for (int k = 0; k < headers.Length; k++)
+            {
+                Console.Write(headers[k]+"\t");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j <= col - 1; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write(array[i, j] + "\t" + "\t");
+                    }
+                    else
+                    {
+                        Console.Write(array[i, j] + "\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
         public static double CalculateSupport(Dictionary<int, string[]> transactions, string Y, string[] X)
         {
             string[] combo = new string[X.Length + 1];
@@ -128,7 +161,7 @@ namespace AprioriAlgorithm
                 }
             }
 
-             double confidence = countXuY / countX;
+            double confidence = countXuY / countX;
             Console.WriteLine("Confidence: " + confidence);
             return confidence;
         }
